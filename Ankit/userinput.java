299969e384436491f0e185f44f0beafda662133c
ankitgpt9798 <amitankitgupta1@gmail.com>
2025-06-26 16:51:48 +0530

    Update

---
+++ Ankit/userinput.java
@@ -0,0 +1,13 @@
+
+
+
+import java.util.Scanner;
+public class userinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number : ");
+        int n = sc.nextInt();
+        System.out.print("the square of the number is : ");
+        System.out.print(n*n);
+
+    }}
